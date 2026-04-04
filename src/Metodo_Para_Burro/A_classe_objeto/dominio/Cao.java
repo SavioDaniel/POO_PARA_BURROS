f4aa00097d877b0c5a0131058d3085973c706d6b
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-04 15:46:32 -0300

    Initial commit

---
+++ src/Metodo_Para_Burro/A_classe_objeto/dominio/Cao.java
@@ -0,0 +1,9 @@
+package Metodo_Para_Burro.A_classe_objeto.dominio;
+
+public class Cao {
+    public String name;
+    public String breed;
+    public int age;
+
+    
+}
---
+++ src/Metodo_Para_Burro/A_classe_objeto/test/CaoTest.java
@@ -0,0 +1,4 @@
+package Metodo_Para_Burro.A_classe_objeto.test;
+
+public class CaoTest {
+}
