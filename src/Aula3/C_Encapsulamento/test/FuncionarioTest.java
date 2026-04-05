ef2d700c6b52588421eb463b09b14c04af6361c8
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-05 14:42:20 -0300

    Update FuncionarioTest.java

--- src/Aula3/C_Encapsulamento/test/FuncionarioTest.java
+++ src/Aula3/C_Encapsulamento/test/FuncionarioTest.java
@@ -1,4 +1,11 @@
 package Aula3.C_Encapsulamento.test;
 
+import Aula3.C_Encapsulamento.dominio.Funcionario;
+
 public class FuncionarioTest {
+    static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Sávio", -24, "609.201.693-17");
+        System.out.println(funcionario.toString());
+        
+    }
 }
