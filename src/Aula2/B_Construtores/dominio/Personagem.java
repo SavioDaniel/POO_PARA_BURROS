f4dab45036f7eedaef2af43f59aaa2e7f0a6467c
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-05 11:09:54 -0300

    Update Personagem.java

--- src/Aula2/B_Construtores/dominio/Personagem.java
+++ src/Aula2/B_Construtores/dominio/Personagem.java
@@ -1,6 +1,22 @@
 package Aula2.B_Construtores.dominio;
 
 public class Personagem {
+
     public String name;
-    public String
+    public String breed;
+    public String job;
+
+    public Personagem(String name, String breed, String job) {
+        this.name = name;
+        this.breed = breed;
+        this.job = job;
+    }
+
+    public void Carnivore (){
+        System.out.println("I am a meat eater.");
+    }
+
+    public void Herbivore(){
+        System.out.println("I am a tree eater.");
+    }
 }
