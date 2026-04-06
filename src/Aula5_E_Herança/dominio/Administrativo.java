f8607e64adfe99fdb7021d35f1a7bd60cc430e21
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-06 14:57:41 -0300

    Create Administrativo.java

---
+++ src/Aula5_E_Herança/dominio/Administrativo.java
@@ -0,0 +1,36 @@
+package Aula5_E_Herança.dominio;
+
+public class Administrativo extends Assistente {
+    private String turno;
+    private double adicionalNoturno;
+
+    public Administrativo(String nome, double salario, String numeroDeMatricula, String turno, double adicionalNoturno) {
+        super(nome, salario, numeroDeMatricula);
+        this.turno = turno;
+        this.adicionalNoturno = adicionalNoturno;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public double getAdicionalNoturno() {
+        return adicionalNoturno;
+    }
+
+    public void setAdicionalNoturno(double adicionalNoturno) {
+        this.adicionalNoturno = adicionalNoturno;
+    }
+
+    @Override
+    public double ganhoAnual() {
+        if (turno.equalsIgnoreCase("noite")) {
+            return super.ganhoAnual() + (adicionalNoturno * 12);
+        }
+        return super.ganhoAnual();
+    }
+}
