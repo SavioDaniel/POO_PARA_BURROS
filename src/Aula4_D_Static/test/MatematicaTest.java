846ebca67795b101148396735a4e47a7ed5c1967
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-06 13:10:32 -0300

    Objeto
    Não precisa criar objeto para acessar.

---
+++ src/Aula4_D_Static/test/MatematicaTest.java
@@ -0,0 +1,16 @@
+package Aula4_D_Static.test;
+
+import Aula4_D_Static.dominio.Matematica;
+
+public class MatematicaTest {
+    static void main(String[] args) {
+        System.out.println(Matematica.somar(3,7));
+
+        System.out.println(Matematica.areaDoQuadrado(18, 49));
+
+        System.out.println(Matematica.areaCirculo(36));
+
+
+
+    }
+}
