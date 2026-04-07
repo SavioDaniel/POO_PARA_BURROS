d67a57f21031b4fa75ea8dba9608f043a4649eb0
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-06 22:19:26 -0300

    Classe FIlho

---
+++ src/Aula52_E_Herança/dominio/Cat.java
@@ -0,0 +1,12 @@
+package Aula52_E_Herança.dominio;
+
+public class Cat extends Animal{
+    public Cat(String name, String breed) {
+        super(name, breed);
+    }
+
+    @Override
+    public String emitirSom() {
+        return "MIAU MIAU MIAU";
+    }
+}
