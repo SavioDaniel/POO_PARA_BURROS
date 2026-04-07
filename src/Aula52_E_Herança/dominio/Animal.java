ce8e9105ee2ad4b7996a60d0dcf303508355802a
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-06 22:19:09 -0300

    Classe Pai

---
+++ src/Aula52_E_Herança/dominio/Animal.java
@@ -0,0 +1,27 @@
+package Aula52_E_Herança.dominio;
+
+public class Animal {
+    protected String name;
+    protected String breed;
+
+    public Animal(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+    public String emitirSom() {
+        return "Som genérico";
+    }
+
+    public String caminhar() {
+        return name + " está caminhando";
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+
+}
