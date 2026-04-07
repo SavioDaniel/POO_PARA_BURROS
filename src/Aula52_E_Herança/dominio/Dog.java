3d0a0a7d42643683c28ec2aff696d308ae9c1c1f
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-06 22:19:35 -0300

    Classe Filho

---
+++ src/Aula52_E_Herança/dominio/Dog.java
@@ -0,0 +1,13 @@
+package Aula52_E_Herança.dominio;
+
+public class Dog extends Animal{
+    public Dog(String name, String breed) {
+        super(name, breed);
+    }
+
+    @Override
+    public String emitirSom() {
+        return  "AU AU AU AU";
+    }
+
+}
