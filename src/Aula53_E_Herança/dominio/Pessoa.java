6dbc69a78b73a4c2b43f025ed1cb4f8b20c5c814
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-08 15:21:20 -0300

    Create Pessoa.java

---
+++ src/Aula53_E_Herança/dominio/Pessoa.java
@@ -0,0 +1,23 @@
+package Aula53_E_Herança.dominio;
+
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String Ocupacao(){
+        return "Faz algo";
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", idade=" + idade + "]";
+    }
+
+
+
+}
