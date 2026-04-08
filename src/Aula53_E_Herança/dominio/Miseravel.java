2cf93228b0767e9f4e705782b1b9a8f983db8e54
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-08 15:21:17 -0300

    FIlha Miseravel

---
+++ src/Aula53_E_Herança/dominio/Miseravel.java
@@ -0,0 +1,15 @@
+package Aula53_E_Herança.dominio;
+
+public class Miseravel extends Pessoa {
+
+    public Miseravel(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    @Override
+    public String Ocupacao() {
+        // TODO Auto-generated method stub
+        return "Mendiga";
+    }
+
+}
