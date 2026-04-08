bf7f3d6141f2c3d175d65a837d2c4a80591bb07c
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-08 15:25:05 -0300

    Create Assistente.java

---
+++ src/Aula5EHerenca/Aula5_E_Herança/dominio/Assistente.java
@@ -0,0 +1,24 @@
+package Aula5EHerenca.Aula5_E_Herança.dominio;
+
+public class Assistente extends Funcionario{
+    private String numeroDeMatricula;
+
+    public Assistente(String nome, double salario, String numeroDeMatricula) {
+        super(nome, salario);
+        this.numeroDeMatricula = numeroDeMatricula;
+    }
+
+    @Override
+    public void exibeDados() {
+        super.exibeDados();
+        System.out.println("Número de matricula: " + this.numeroDeMatricula);
+    }
+
+    public String getNumeroDeMatricula() {
+        return numeroDeMatricula;
+    }
+
+    public void setNumeroDeMatricula(String numeroDeMatricula) {
+        this.numeroDeMatricula = numeroDeMatricula;
+    }
+}
