f09c11362ad240c978e16208700c5d888b93b3ad
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-08 15:21:29 -0300

    Create PessoaTest.java

---
+++ src/Aula53_E_Herança/test/PessoaTest.java
@@ -0,0 +1,22 @@
+package Aula53_E_Herança.test;
+
+
+import Aula53_E_Herança.dominio.Miseravel;
+import Aula53_E_Herança.dominio.Pessoa;
+import Aula53_E_Herança.dominio.Pobre;
+import Aula53_E_Herança.dominio.Rico;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        Pessoa rica = new Rico("Savio", 24, 25000);
+        Pessoa pobre = new Pobre("Rebeca", 20);
+        Pessoa miseravel = new Miseravel("Claudio", 40);
+
+
+        System.out.println(rica.Ocupacao());
+        System.out.println(pobre.Ocupacao());
+        System.out.println(miseravel.Ocupacao());
+    }
+
+}
+
