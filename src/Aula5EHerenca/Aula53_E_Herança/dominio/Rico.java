364163f8cabd619c847af85ada0b2adcebc50691
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-08 15:25:08 -0300

    Create Rico.java

---
+++ src/Aula5EHerenca/Aula53_E_Herança/dominio/Rico.java
@@ -0,0 +1,25 @@
+package Aula5EHerenca.Aula53_E_Herança.dominio;
+
+public class Rico extends Pessoa{
+    private double dinheiro;
+
+    public Rico(String nome, int idade, double dinheiro) {
+        super(nome, idade);
+        this.dinheiro = dinheiro;
+    }
+
+    public double getDinheiro() {
+        return dinheiro;
+    }
+
+    public void setDinheiro(double dinheiro) {
+        this.dinheiro = dinheiro;
+    }
+
+    @Override
+    public String Ocupacao() {
+        return "Faz Compras";
+    }
+
+}
+
