7725453fd67a68c44beaa8b900032c39ebea2937
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-09 18:56:52 -0300

    Classe Pai

---
+++ src/Aula5EHerenca/Aula54/dominio/Veiculo.java
@@ -0,0 +1,46 @@
+package Aula5EHerenca.Aula54.dominio;
+
+public class Veiculo {
+    protected String marca;
+    protected String modelo;
+    protected double velocidade;
+
+    //Vai aumentar a velocidade conforme for acelerando
+    public double acelerar(double aumentar){
+        return velocidade += aumentar ;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", velocidade=" + velocidade +
+                '}';
+    }
+
+    //Get e set
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+}
