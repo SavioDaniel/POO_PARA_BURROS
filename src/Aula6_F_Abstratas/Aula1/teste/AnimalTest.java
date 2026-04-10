1cd582e61b9ef164bbca5026c5e24e955075de64
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-10 14:02:08 -0300

    Update AnimalTest.java

--- src/Aula6_F_Abstratas/Aula1/teste/AnimalTest.java
+++ src/Aula6_F_Abstratas/Aula1/teste/AnimalTest.java
@@ -1,4 +1,21 @@
 package Aula6_F_Abstratas.Aula1.teste;
 
+import Aula6_F_Abstratas.Aula1.dominio.Cao;
+import Aula6_F_Abstratas.Aula1.dominio.Gato;
+
+
 public class AnimalTest {
+    public static void main(String[] args) {
+
+        Cao cao = new Cao("Billy");
+        Gato gato = new Gato("Chana");
+
+        System.out.println(cao);
+        cao.fazerSom();
+
+        System.out.println("------------------------");
+
+        System.out.println(gato);
+        gato.fazerSom();
+    }
 }
