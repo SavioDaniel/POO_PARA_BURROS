43489a56b2f9b787b3314a0a4096322316e912ef
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-10 00:11:00 -0300

    Update Carro.java

--- src/Aula5EHerenca/Aula54/dominio/Carro.java
+++ src/Aula5EHerenca/Aula54/dominio/Carro.java
@@ -3,6 +3,11 @@ package Aula5EHerenca.Aula54.dominio;
 public class Carro extends Veiculo{
     private int qtdPortas;
 
+    public Carro(String marca, String modelo, double velocidade, int qtdPortas) {
+        super(marca, modelo, velocidade);
+        this.qtdPortas = qtdPortas;
+    }
+
     @Override
     public String toString() {
         return super.toString() +
