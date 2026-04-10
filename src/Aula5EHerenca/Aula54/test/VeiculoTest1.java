bf13cebbe68e5a213259bbb7ccfea1466f921568
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-10 00:11:07 -0300

    Update VeiculoTest1.java

--- src/Aula5EHerenca/Aula54/test/VeiculoTest1.java
+++ src/Aula5EHerenca/Aula54/test/VeiculoTest1.java
@@ -1,7 +1,19 @@
 package Aula5EHerenca.Aula54.test;
 
+import Aula5EHerenca.Aula54.dominio.Carro;
+import Aula5EHerenca.Aula54.dominio.Moto;
+import Aula5EHerenca.Aula54.dominio.Veiculo;
+
 public class VeiculoTest1 {
-    static void main(String[] args) {
-        
+    public static void main(String[] args) {
+
+        Veiculo carro = new Carro("Fiat", "Uno", 120, 4);
+        Veiculo moto = new Moto("Honda", "Fan", 140, 160);
+
+        carro.acelerar(20);
+        moto.acelerar(30);
+
+        System.out.println(carro);
+        System.out.println(moto);
     }
 }
