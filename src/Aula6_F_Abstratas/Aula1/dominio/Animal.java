62465046651675926b399b06a761e94931aedfec
SavioDaniel <120289077+SavioDaniel@users.noreply.github.com>
2026-04-10 14:02:00 -0300

    Update Animal.java

--- src/Aula6_F_Abstratas/Aula1/dominio/Animal.java
+++ src/Aula6_F_Abstratas/Aula1/dominio/Animal.java
@@ -1,4 +1,16 @@
 package Aula6_F_Abstratas.Aula1.dominio;
 
-public class Animal {
+public abstract class Animal {
+    protected String nome;
+
+    public Animal(String nome) {
+        this.nome = nome;
+    }
+
+    public abstract void fazerSom();
+
+    @Override
+    public String toString() {
+        return nome + ", está dormindo";
+    }
 }
